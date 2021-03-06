097c72d530205a82e6deacfda75a296bafab080e
narms <75282394+n-arms@users.noreply.github.com>
2021-03-06 11:22:30 -0500

    Implement tick function (use threading)

--- animals.py
+++ animals.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
 from random import random, randint
 from math import sqrt
 from collections import OrderedDict
 from threading import Thread
+from time import sleep
+from sys import exit
 
 class Position:
     """reprsent a Position in 2d space
@@ -18,7 +21,7 @@ class Position:
     def rand(lower, upper):
         return Position(randint(lower, upper), randint(lower, upper))
     def __str__(self):
-        return "NULL" if self._is_null else f"({self._x}, {self._y})"
+        return "NULL" if self._is_null else f"({self._x:.2f}, {self._y:.2f})"
     def __add__(self, other):
         if self._is_null:
             return Position.null()
@@ -48,8 +51,16 @@ class Position:
         if self._is_null:
             return Position.null()
         length = self.distance(0, 0)
+        if (length == 0):
+            return Position(0, 0)
         return Position(self._x/length, self._y/length)
-
+    def nearest(self, others):
+        if len(others) == 0:
+            return Position.null()
+        output = others[0]
+        for i in range(1, len(others)):
+            output = output if output.distance(self) < others[i].distance(self) else others[i]
+        return output
 
 class Beaver:
     """represent a Beaver, with a method to call every gametick"""
@@ -62,30 +73,30 @@ class Beaver:
         self._thirst = 0.5
         self._target = Position.null()
     def __str__(self):
-        return f"Beaver: [hunger: {self._hunger}, thirst: {self._thirst}, position: {self._pos}, state: {self._state}]"
+        return f"Beaver: [hunger: {self._hunger:.3f}, thirst: {self._thirst:.3f}, position: {self._pos}, state: {self._state:.3f}]"
     def tick(self, gameworld):
         self._hunger += 0.01
         self._thirst += 0.01
         if (self._hunger > 1 or self._thirst > 1):
             gameworld.kill(self)
         if (self._state == 0):
-            self.chose_state(gameworld.food_water())
+            self._chose_state(gameworld.food_water(self._pos))
         elif (self._state == 1):
             self._remaining_ticks -= 1
             if (self._remaining_ticks == 0):
-                self.eat()
+                self._eat()
         elif (self._state == 2):
             self._remaining_ticks -= 1
             if (self._remaining_ticks == 1):
-                self.drink()
+                self._drink()
         elif self._state in (3, 4):
-            self.advance()
+            self._advance()
         else:
             raise ValueError("illegal state")
 
     def _move(self):
         self._pos += (self._target - self._pos).normalize()
-        return self._pos.distance(self._target) < 2
+        return self._pos.distance(self._target) > 2
 
     def _chose_state(self, features):
         if "food" in features:
@@ -112,10 +123,10 @@ class Beaver:
     def _drink(self):
         self._thirst = max(0, self._thirst - 0.25)
     def _advance(self):
-        result = self.move()
+        result = self._move()
         if result == 0:
             self._state = 2
-            self._remaining_ticks == 5
+            self._remaining_ticks = 5
         elif result == 1:
             self._state = 0
 
@@ -126,7 +137,7 @@ class World:
         self._water = [Position.rand(0, 100) for i in range(num_water)]
         self._tick_time = 0.5
     def mainloop(self):
-        pass
+        self._loop()
     def _loop(self):
         while True:
             tick_thread = Thread(target = self._tick)
@@ -135,8 +146,27 @@ class World:
             tick_thread.join() #join threads
             self.render_beavers() #render beavers
     def _tick(self):
-        time.sleep(self._tick_time)
+        sleep(self._tick_time)
+        print("========= GAMETICK =========")
+        for beaver in self._beavers:
+            beaver.tick(self)
+            print(beaver)
+        if len(self._beavers) == 0:
+            exit()
+        print("\n\n\n\n")
     def render_background(self):
         pass
     def render_beavers(self):
         pass
+    def food_water(self, position):
+        return {"food": position.nearest(self._food), "water": position.nearest(self._water)}
+    def kill(self, animal):
+        if isinstance(animal, Beaver):
+            for i in range(len(self._beavers)):
+                if self._beavers[i] is animal:
+                    self._beavers.pop(i)
+                    return
+
+
+w = World(5, 20, 20)
+w.mainloop()
