5aea159755779f4f0a1f07c498c812896321796d
narms <75282394+n-arms@users.noreply.github.com>
2021-03-06 13:25:56 -0500

    Add WorldCanvas to render in beavers and food/water

--- animals.py
+++ animals.py
@@ -2,9 +2,7 @@
 from random import random, randint
 from math import sqrt
 from collections import OrderedDict
-from threading import Thread
-from time import sleep
-from sys import exit
+
 
 class Position:
     """reprsent a Position in 2d space
@@ -61,6 +59,8 @@ class Position:
         for i in range(1, len(others)):
             output = output if output.distance(self) < others[i].distance(self) else others[i]
         return output
+    def image_coords(self):
+        return (self._x, self._y, self._x+1, self._y+1)
 
 class Beaver:
     """represent a Beaver, with a method to call every gametick"""
@@ -72,10 +72,10 @@ class Beaver:
         self._hunger = 0.5
         self._thirst = 0.5
         self._target = Position.null()
+        self._speed = 3
     def __str__(self):
         return f"Beaver: [hunger: {self._hunger:.3f}, thirst: {self._thirst:.3f}, position: {self._pos}, state: {self._state:.3f}]"
     def tick(self, gameworld):
-        print("ticking: "+str(self))
         self._hunger += 0.01
         self._thirst += 0.01
         if (self._hunger > 1 or self._thirst > 1):
@@ -97,7 +97,7 @@ class Beaver:
             raise ValueError("illegal state")
 
     def _move(self):
-        self._pos += (self._target - self._pos).normalize()
+        self._pos += (self._target - self._pos).normalize() * self._speed
         return self._pos.distance(self._target) > 2
 
     def _chose_state(self, features):
@@ -139,49 +139,5 @@ class Beaver:
             self._remaining_ticks = 5
         elif result == 1:
             self._state = 0
-
-class World:
-    def __init__(self, num_beavers, num_food, num_water):
-        self._beavers = [Beaver(Position.rand(0, 100)) for i in range(num_beavers)]
-        self._food = [Position.rand(0, 100) for i in range(num_food)]
-        self._water = [Position.rand(0, 100) for i in range(num_water)]
-        self._tick_time = 0.5
-    def mainloop(self):
-        self._loop()
-    def _loop(self):
-        while True:
-            tick_thread = Thread(target = self._tick)
-            tick_thread.start() #tick all of the beavers
-            self.render_background() #render background while waiting
-            tick_thread.join() #join threads
-            self.render_beavers() #render beavers
-    def _tick(self):
-        sleep(self._tick_time)
-        print("========= GAMETICK =========")
-        print(f"beavers: {len(self._beavers)}, water: {len(self._water)}, food: {len(self._food)}")
-        for beaver in self._beavers:
-            beaver.tick(self)
-        if len(self._beavers) == 0:
-            exit()
-        print("\n\n\n\n")
-    def render_background(self):
-        pass
-    def render_beavers(self):
-        pass
-    def food_water(self, position):
-        return {"food": position.nearest(self._food), "water": position.nearest(self._water)}
-    def kill(self, animal):
-        if isinstance(animal, Beaver):
-            for i in range(len(self._beavers)):
-                if self._beavers[i] is animal:
-                    self._beavers.pop(i)
-                    return
-    def consume(self, resource, position):
-        if (resource == "water"):
-            self._water.remove(position)
-        else:
-            self._food.remove(position)
-
-
-w = World(1, 20, 20)
-w.mainloop()
+    def get_coords(self):
+        return self._pos.image_coords()
---
+++ sim.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+from animals import Beaver, Position
+from threading import Thread
+from time import sleep
+from sys import exit
+from tkinter import *
+
+class WorldCanvas(Canvas):
+    def __init__(self, master):
+        Canvas.__init__(self, master, bg="green", width=500, height=500)
+        self._beavers = []
+        self._food = []
+        self._water = []
+    def _reset_beavers(self):
+        for i in self._beavers:
+            self.delete(i)
+        self._beavers = []
+
+    def _reset_background(self):
+        for i in self._water:
+            self.delete(i)
+        for i in self._food:
+            self.delete(i)
+        self._food = []
+        self._water = []
+    def redraw_beavers(self, beavers):
+        self._reset_beavers()
+        for beaver in beavers:
+            self._beavers.append(self.create_rectangle(*[i*5 for i in beaver.get_coords()]))
+    def redraw_background(self, food, water):
+        self._reset_background()
+        for tree in food:
+            self._food.append(self.create_oval(*[i*5 for i in tree.image_coords()]))
+        for pool in water:
+            self._water.append(self.create_oval(*[i*5 for i in pool.image_coords()]))
+
+class World:
+    def __init__(self, num_beavers, num_food, num_water, master):
+        self._beavers = [Beaver(Position.rand(0, 100)) for i in range(num_beavers)]
+        self._food = [Position.rand(0, 100) for i in range(num_food)]
+        self._water = [Position.rand(0, 100) for i in range(num_water)]
+        self._tick_time = 0.5
+
+        self.frame = Frame(root)
+        self.frame.grid()
+        self.canvas = WorldCanvas(root)
+        self.canvas.grid()
+        self.render_background()
+        self.render_beavers()
+
+        self.mainloop()
+
+    def mainloop(self):
+        tick_thread = Thread(target = self._tick)
+        tick_thread.start() #tick all of the beavers
+        self.render_background() #render background while waiting
+        tick_thread.join() #join threads
+        self.render_beavers() #render beavers
+        self.frame.after(10, self.mainloop)
+
+    def _tick(self):
+        sleep(self._tick_time)
+        print("========= GAMETICK =========")
+        for beaver in self._beavers:
+            beaver.tick(self)
+        if len(self._beavers) == 0:
+            exit()
+    def render_background(self):
+        self.canvas.redraw_background(self._food, self._water)
+    def render_beavers(self):
+        self.canvas.redraw_beavers(self._beavers)
+    def food_water(self, position):
+        return {"food": position.nearest(self._food), "water": position.nearest(self._water)}
+    def kill(self, animal):
+        if isinstance(animal, Beaver):
+            for i in range(len(self._beavers)):
+                if self._beavers[i] is animal:
+                    self._beavers.pop(i)
+                    return
+    def consume(self, resource, position):
+        if (resource == "water"):
+            self._water.remove(position)
+        else:
+            self._food.remove(position)
+
+
+root = Tk()
+w = World(5, 20, 20, root)
+root.mainloop()
