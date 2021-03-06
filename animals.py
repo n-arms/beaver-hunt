0c580decf84451717a769ea038d25545f840d3f6
narms <75282394+n-arms@users.noreply.github.com>
2021-03-06 10:26:03 -0500

    Add World class to rep the gameworld

--- animals.py
+++ animals.py
@@ -1,6 +1,7 @@
-from random import random
+from random import random, randint
 from math import sqrt
 from collections import OrderedDict
+from threading import Thread
 
 class Position:
     """reprsent a Position in 2d space
@@ -13,6 +14,9 @@ class Position:
     @staticmethod
     def null():
         return Position(0, 0, True)
+    @staticmethod
+    def rand(lower, upper):
+        return Position(randint(lower, upper), randint(lower, upper))
     def __str__(self):
         return "NULL" if self._is_null else f"({self._x}, {self._y})"
     def __add__(self, other):
@@ -65,7 +69,7 @@ class Beaver:
         if (self._hunger > 1 or self._thirst > 1):
             gameworld.kill(self)
         if (self._state == 0):
-            self.chose_state(gameworld.features())
+            self.chose_state(gameworld.food_water())
         elif (self._state == 1):
             self._remaining_ticks -= 1
             if (self._remaining_ticks == 0):
@@ -79,11 +83,11 @@ class Beaver:
         else:
             raise ValueError("illegal state")
 
-    def move(self):
+    def _move(self):
         self._pos += (self._target - self._pos).normalize()
         return self._pos.distance(self._target) < 2
 
-    def chose_state(self, features):
+    def _chose_state(self, features):
         if "food" in features:
             if "water" in features:
                 if self._hunger > self._thirst:
@@ -103,14 +107,36 @@ class Beaver:
                 self._target = Position.null()
                 self._state = 0
 
-    def eat(self):
+    def _eat(self):
         self._hunger = max(0, self._hunger - 0.25)
-    def drink(self):
+    def _drink(self):
         self._thirst = max(0, self._thirst - 0.25)
-    def advance(self):
+    def _advance(self):
         result = self.move()
         if result == 0:
             self._state = 2
             self._remaining_ticks == 5
         elif result == 1:
             self._state = 0
+
+class World:
+    def __init__(self, num_beavers, num_food, num_water):
+        self._beavers = [Beaver(Position.rand(0, 100)) for i in range(num_beavers)]
+        self._food = [Position.rand(0, 100) for i in range(num_food)]
+        self._water = [Position.rand(0, 100) for i in range(num_water)]
+        self._tick_time = 0.5
+    def mainloop(self):
+        pass
+    def _loop(self):
+        while True:
+            tick_thread = Thread(target = self._tick)
+            tick_thread.start() #tick all of the beavers
+            self.render_background() #render background while waiting
+            tick_thread.join() #join threads
+            self.render_beavers() #render beavers
+    def _tick(self):
+        time.sleep(self._tick_time)
+    def render_background(self):
+        pass
+    def render_beavers(self):
+        pass
