38a9a31bc8bd239f5aefc180a7946e034842a7de
narms <75282394+n-arms@users.noreply.github.com>
2021-03-06 15:57:24 -0500

    Implement basic 1d noise

--- noise.py
+++ noise.py
@@ -1,16 +1,19 @@
 from tkinter import *
 from random import random
+from math import floor, ceil
 
 class NoiseSpace:
-    def __init__(self, size):
-        self.points = [random() for i in range(size)]
+    def __init__(self, size, amp, freq):
+        self.points = [random()*amp for i in range(size)]
+        self.amp = amp
+        self.freq = freq
     def get(self, x):
-        if isinstance(x, int):
-            return self.points[x]
-        return NoiseSpace.blend(x//1, self.points[x//1], x//1 + 1, self.points[x//1 + 1], x)
+        if floor(x) == ceil(x):
+            return self.points[int(x)]
+        return NoiseSpace.blend(floor(x), self.points[floor(x)], ceil(x), self.points[ceil(x)], x)
     def render(self, canvas):
-        for i in range(len(self.points)*10):
-            canvas.create_rectangle(i, self.points[i//10]*10+250, (i+0.1), (self.points[i//10]+0.1)*10+250)
+        for i in [i/self.freq for i in range(len(self.points)*self.freq - self.freq)]:
+            canvas.create_rectangle(i*self.freq, self.get(i)+250, i*self.freq+0.1, self.get(i)+250.1)
     @staticmethod
     def blend(x1, y1, x2, y2, a):
         return ((y1 - y2) / (x1 - x2)) * (a - x1) + y1
@@ -21,8 +24,7 @@ f.grid()
 c = Canvas(f, bg="white", width="500", height="500")
 c.grid()
 
-n = NoiseSpace(50)
+n = NoiseSpace(50, 10, 5)
 n.render(c)
-
 
 root.mainloop()
